b0eb82f06a149cda587d140e5a66c034d4f769c4
zbigcheese <34444792+zbigcheese@users.noreply.github.com>
2025-06-28 11:18:19 +0200

    change in way sprinkle is set up

--- src/UfOutsetaIntegration.php
+++ src/UfOutsetaIntegration.php
@@ -2,12 +2,40 @@
 
 namespace Zbigcheese\Sprinkles\UfOutsetaIntegration;
 
-use UserFrosting\Sprinkle\Sprinkle;
+use UserFrosting\Sprinkle\SprinkleRecipe;
+use Zbigcheese\Sprinkles\UfOutsetaIntegration\Routes\WebRoutes;
+use Zbigcheese\Sprinkles\UfOutsetaIntegration\Templates\DefaultTemplate;
 
-class UfOutsetaIntegration implements Sprinkle
+class UfOutsetaIntegration implements SprinkleRecipe
 {
-    public function getBootstrapper(): string
+    public function getName(): string
     {
-        return \Zbigcheese\Sprinkles\UfOutsetaIntegration\Sprinkle\UfOutsetaIntegrationBootstrapper::class;
+        return 'Outseta Integration';
+    }
+
+    public function getPath(): string
+    {
+        return __DIR__ . '/../';
+    }
+
+    public function getSprinkles(): array
+    {
+        // This sprinkle has no dependencies on other sprinkles.
+        // Its dependencies on framework/core are handled by composer.json.
+        return [];
+    }
+
+    public function getRoutes(): array
+    {
+        return [
+            WebRoutes::class,
+        ];
+    }
+
+    public function getTemplates(): array
+    {
+        return [
+            'default' => DefaultTemplate::class,
+        ];
     }
 }
