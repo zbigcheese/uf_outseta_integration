d4edcb92a38117e22d439edfa68d4f13c6c513cf
zbigcheese <34444792+zbigcheese@users.noreply.github.com>
2025-06-28 12:45:03 +0200

    Update default.php

--- config/default.php
+++ config/default.php
@@ -2,7 +2,7 @@
 
 return [
     'outseta' => [
-        'domain'     => 'stout-ux',
+        'domain'     => getenv('OUTSETA_DOMAIN') ?: '',
         'api_key'    => getenv('OUTSETA_API_KEY') ?: '',
         'secret_key' => getenv('OUTSETA_SECRET_KEY') ?: '',
     ],
