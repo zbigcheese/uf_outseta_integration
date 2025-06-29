b310a5637c7468feb3de7b4926f5153b8e49e5a7
zbigcheese <34444792+zbigcheese@users.noreply.github.com>
2025-06-29 08:16:42 +0200

    Update UfOutsetaIntegration.php

--- src/UfOutsetaIntegration.php
+++ src/UfOutsetaIntegration.php
@@ -2,6 +2,9 @@
 
 namespace Zbigcheese\Sprinkles\UfOutsetaIntegration;
 
+use UserFrosting\Sprinkle\Core\Bakery\MigrationRecipe;
+use Zbigcheese\Sprinkles\UfOutsetaIntegration\Database\Migrations\AddOutsetaSubscribersTable;
+
 use UserFrosting\Sprinkle\Core\Core;
 use UserFrosting\Sprinkle\SprinkleRecipe;
 
@@ -47,5 +50,18 @@ class UfOutsetaIntegration implements SprinkleRecipe
     {
         // This sprinkle does not have any custom services.
         return [];
+    }
+
+    /**
+     * Returns a list of all migrations classes for this Sprinkle.
+     * This method is required by the MigrationRecipe interface.
+     *
+     * @return array<class-string<\UserFrosting\Sprinkle\Core\Database\Migration>>
+     */
+    public function getMigrations(): array
+    {
+        return [
+            AddOutsetaSubscribersTable::class,
+        ];
     }
 }
